70706bfc4c0026f7c4d73c18596341044762cca9
surya <suryaa.net.in@gmail.com>
2025-07-01 12:05:32 +0100

    week 1-2 added

---
+++ week1-2.py
@@ -0,0 +1,74 @@
+"""
+Exercise 1. Write a Python function that converts a distance in miles to a distance in
+kilometres. Note that one mile is exactly 25146/15625 km.
+Exercise 2. Write a Python function that takes three inputs (the number of hours since
+midnight, the number of minutes in the hour, and the number of seconds in the minute)
+and returns the number of seconds since midnight.
+Group activity/discussion. Complex numbers are numbers of the form given below (in
+Python). Notice we have 1j and 4j not 1*j and 4*j. Here j is NOT a variable.
+u = 0+1j
+z = 3+4j
+print(u,' and ',z)
+(a) Write Python code to find the type of 𝑢 and 𝑧 in Python?
+(b) What is 𝑢 × 𝑢? How is this useful? Investigate −𝑢 × −𝑢 and see if that is also
+interpretable.
+(c) Compare 𝑢 + 𝑧 and 𝑧 + 𝑢. Also compare 𝑢 × 𝑧 and 𝑧 × 𝑢. What can you conclude?
+(d) Let e = 1+0j and then investigate 𝑒 × 𝑧 and 𝑧 × 𝑒. What can you conclude?
+(e) Can you use + and * with a mixture of integers or floats and complex numbers?
+
+"""
+#Exercise
+# Assign your name to a variable and find the the index or 
+# of your middle name
+# Take your year opf birth or age and compare it with the current year
+# What is the length of your MSc course?
+'''EXERCISE 1
+Write a code to determine if a given year is a leap year
+1993, 2024, 2025
+'''
+
+""" EXERCISE 2
+Write a program that uses FOR loops to calculate the total amount of 
+money you spent on lunch last week
+"""
+
+'''EXERCISE 3
+Write a program that would tell you if you need to purchase a winter coat 
+for the following months: April, August, December and January
+
+'''
+
+'''TAKE HOME ASSIGNMENT
+Write a program to determine if a given string is Palindrome.
+'''
+"""
+Exercise 1. Suppose that the current date is given in a Python string in the form
+DD/MM/YYYY. Write Python code using string indexing to print out the date in the form
+DD Month YYYY, e.g., 20/09/2023 would be printed out as 20 September 2023. Write
+further Python code using the string method split() to do the same task without string
+indexing. Hint: You might consider using if-elif-else or a list of strings.
+Data Science Python Labs (2023/24 Semester 2)
+19
+
+Exercise 2. Suppose you have an automatic wildlife camera. You want the camera to switch
+on if the light level is low (less than 0.01 lux) or if the temperature is above freezing, but not
+if both conditions are True. Write Python code to check these conditions and print a
+message when the camera switches on.
+
+Exercise 3. Write Python code using integer division to convert a given whole number of
+minutes into a list consisting of whole number of days, hours and minutes. For example,
+3456 minutes should result in [2, 9, 36], i.e., 2 days, 9 hours and 36 minutes.
+Group Challenge. Write a Python function to check whether a given string has the same
+number of ‘x’s and ‘o’s. The function must return a Boolean value and be case insensitive.
+The given string can contain any characters.
+Exercise 4. The characters in a Python string are UTF-8 characters (not just ASCII). The
+Python code below defines the six different Chess pieces using UTF-8 characters. Write
+Python code to print out the starting chessboard shown below.
+king = '\u265A'
+queen = '\u265B'
+rook = '\u265C'
+bishop = '\u265D'
+knight = '\u265E'
+pawn = '\u265F
+"""
+# week 1 and 2 ended here all sheets questions --------------------
