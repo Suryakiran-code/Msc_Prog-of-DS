66c8cc0a142084c29619a257fb53f81480e13712
surya <suryaa.net.in@gmail.com>
2025-07-04 11:52:48 +0100

    week 5

---
+++ Theory.py
@@ -0,0 +1,30 @@
+# welcome to week 5 classwork-----
+# what is data science 
+# A process of using data to understand things 
+# .data tell a story 
+#.uncover insights, patterns, and trends 
+# transforming data into kn owledge that can guide decesions 
+# secientific apply the scientific method 
+
+# sum of its parts 
+"""
+ARTIFICIAL INTELLIGENCE 
+MACHINE LEARNING 
+MATHAMATICS AND STATISTICS 
+DATA ANALYTICS
+DATA ENGINEERING 
+DATA VISUALISATION
+ALGORITHMS AND INFRASTRUCTURE
+DATA STORAGE/RETRIVEL
+DOMAIN/BUSINESS KNOWLEDGE
+"""
+
+#Data science project life cycle 
+#1.Obtain - what data should be collected
+#2.Scrub - checking, correcting, organising, aggregating, selecting, normalizing(3nf)
+#3.Explore - plots 
+#4.Model-explain predict classify
+#5.Interpret - give advice 
+
+
+
---
+++ week5.py
@@ -0,0 +1,13 @@
+"""
+Introduction to numpy and matplotlib
+.python list vs numpy array
+numpy data types and attributes
+index, slice, stack, split
+vector operations and aggregations 
+Basic plots bar,line,scatterplot
+Time series concepts
+"""
+# performing calculations on data stored in list is far slow 
+# instead we use fast calculations in arrays by ******Numpy****
+# **matplotlib** - easy way to build simple graphical plots like bar,line,scatterplots
+
