45251ddd63203e0fa287664354da9c1f4d0fbb45
Surya <suryaa.net.in@gmail.com>
2025-07-09 10:51:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# welcome main page in git
+MSC DATA SCIENCE final project - proposal - final submissions - 50 credits
+Data - structured, semi-structured, un-structured
+do projects with unstructured data
+not alloweed to use kaggle dataset
+Drugbank - pharamautical data - data set to use in projects
+national library of medicine - data set to use in projects
